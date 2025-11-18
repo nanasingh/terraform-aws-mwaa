c919051a3804401a62db4a4034a0b843c62f22bc
nanasingh <nana@withmoment.com>
2025-11-18 11:33:13 -0500

    fix permissions to match current commit we're using

--- data.tf
+++ data.tf
@@ -38,13 +38,6 @@ data "aws_iam_policy_document" "mwaa_assume" {
       type        = "Service"
       identifiers = ["s3.amazonaws.com"]
     }
-    dynamic "principals" {
-      for_each = var.additional_principal_arns
-      content {
-        type        = "AWS"
-        identifiers = [principals.value]
-      }
-    }
   }
 }
 #tfsec:ignore:AWS099
@@ -62,9 +55,7 @@ data "aws_iam_policy_document" "mwaa" {
   statement {
     effect = "Allow"
     actions = [
-      "s3:GetObject*",
-      "s3:GetBucket*",
-      "s3:List*"
+      "s3:*"
     ]
     resources = [
       local.source_bucket_arn,
@@ -93,8 +84,9 @@ data "aws_iam_policy_document" "mwaa" {
     actions = [
       "logs:DescribeLogGroups",
       "cloudwatch:PutMetricData",
-      "s3:GetAccountPublicAccessBlock",
-      "eks:DescribeCluster"
+      "batch:DescribeJobs",
+      "batch:ListJobs",
+      "eks:*"
     ]
     resources = [
       "*"
@@ -116,10 +108,6 @@ data "aws_iam_policy_document" "mwaa" {
     ]
   }
 
-  # See note in https://docs.aws.amazon.com/mwaa/latest/userguide/mwaa-create-role.html
-  # if MWAA is using a AWS managed KMS key, we have to give permission to the key in ?? account
-  # We don't know what account AWS puts their key in so we use not_resources to grant access to all
-  # accounts except for ours
   dynamic "statement" {
     for_each = var.kms_key != null ? [] : [1]
     content {
